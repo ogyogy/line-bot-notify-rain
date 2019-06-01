541c21205d0d21cf07c2ae1906271674f7fa0a95
ogyogy <47146269+ogyogy@users.noreply.github.com>
2019-06-02 05:24:18 +0900

    project files

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+const line = require('@line/bot-sdk');
+const crypto = require('crypto');
+const client = new line.Client({ channelAccessToken: process.env.ACCESSTOKEN });
+
+exports.handler = function (event, context) {
+    let signature = crypto.createHmac('sha256', process.env.CHANNELSECRET).update(event.body).digest('base64');
+    let checkHeader = (event.headers || {})['X-Line-Signature'];
+    let body = JSON.parse(event.body);
+    if (signature === checkHeader) {
+        if (body.events[0].replyToken === '00000000000000000000000000000000') { //接続確認エラー回避
+            let lambdaResponse = {
+                statusCode: 200,
+                headers: { "X-Line-Status": "OK" },
+                body: '{"result":"connect check"}'
+            };
+            context.succeed(lambdaResponse);
+        } else {
+            let text = body.events[0].message.text;
+            const message = {
+                'type': 'text',
+                'text': text
+            };
+            client.replyMessage(body.events[0].replyToken, message)
+                .then((response) => {
+                    let lambdaResponse = {
+                        statusCode: 200,
+                        headers: { "X-Line-Status": "OK" },
+                        body: '{"result":"completed"}'
+                    };
+                    context.succeed(lambdaResponse);
+                }).catch((err) => console.log(err));
+        }
+    } else {
+        console.log('Unauthorized');
+    }
+};
